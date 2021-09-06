5b75845fef2980efd0f19cac2348c346bb55a0a7
Mel <meljohnpualon@gmail.com>
2021-09-07 07:26:50 +0800

    added inline style via variable set

--- src/App.js
+++ src/App.js
@@ -27,7 +27,13 @@ const Book = () => {
 const CoverImg = () =>  <img src="https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?cs=srgb&dl=pexels-thisisengineering-3861969.jpg&fm=jpg" className="img" alt="this is a puppy" />
 
 const Title = () => <h1 style={{fontSize: '18px', marginBottom: '10px'}}>The foundation of react components</h1>
-const Author = () => <p>by: Mel John Pualon</p>
+
+const authorStyle = {
+  color: '#747474',
+  letterSpacing: '1px'
+}
+
+const Author = () => <p style={authorStyle}>by: Mel John Pualon</p>
 
 const Person = () => <h3>This is Mel</h3>
 const Message = () => <p>This is my message Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, reprehenderit.</p>
