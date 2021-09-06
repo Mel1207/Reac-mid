d1ceb5082739a308fc5c6e80b58a992bace93a02
Mel <meljohnpualon@gmail.com>
2021-09-07 06:12:22 +0800

    created another sample component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+function Greetings() {
   return ( 
-    <div className="App">
-      <h1>Hi this is Melsasdfee</h1>
-    </div>
+    <section>
+      <h1>Hi this is Melasdfas</h1>
+      <Person />
+      <Message />
+    </section>
   );
 }
 
-export default App;
+const Person = () => <h3>This is Mel</h3>
+const Message = () => <p>This is my message Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, reprehenderit.</p>
+
+export default Greetings;
