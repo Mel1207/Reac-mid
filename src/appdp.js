3a7f039d753c346b0e31ee67459769c143b83e9c
Mel <meljohnpualon@gmail.com>
2021-09-09 00:52:47 +0800

    added importe and exported data values from external file or module

--- src/appdp.js
+++ src/appdp.js
@@ -1,10 +1,19 @@
+// 2 typs of import - named and default import/export
+// only 1 default export per module
+// think of module as a file
+
 import React from "react";
+import { name, age, person } from "./data";
 import './styles.css';
 
 function App () {
     return(
-        <div>Hey there test!</div>
-
+        <div>
+            <p>This is my content</p>
+            <h1>Name: {name}</h1>
+            <h2>Age: {age}</h2>
+            <p>{person.name}</p>
+        </div>
     );
 }
 
---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const name = 'Mel';
+export const age = '26';
+export const person = {
+    name: 'Peter',
+    age: 25
+}
+
+const secretValue = "secret";
