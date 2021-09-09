fabd66dfbccef2d9129b1dad880373ad569838c1
Mel <meljohnpualon@gmail.com>
2021-09-10 00:33:49 +0800

    added event hanlder

--- src/Book.js
+++ src/Book.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react'
 
 export default class Book extends Component {
+    constructor(props) {
+        super(props)
+        // this.handleClic = this.handleClic.bind(this)
+
+        this.state={
+            count: 1
+        }
+    }
+
+ 
+
+    // handleClic() {
+    //     console.log('clicked')
+    //     console.log(this.state.count)
+    // }
+    handleClic = () => {
+        console.log('clicked!')
+        console.log(this.state.count)
+    }
+
     render() {
-        console.log(this.props);
+        // console.log(this.props);
         const {img, book, author} = this.props.info;
 
         return (
@@ -12,6 +32,7 @@ export default class Book extends Component {
                 <div>
                     <h4>Title: {book} </h4>
                     <h5>Author: {author} </h5>
+                    <button onClick={this.handleClic}>Add count</button>
                 </div>
             </div>
         )
